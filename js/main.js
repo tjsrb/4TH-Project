704b8198986534ecebc8543c26190b4fec5a1531
answhdgkr95 <49180647+answhdgkr95@users.noreply.github.com>
2021-04-19 10:28:49 +0900

    test

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const $hashtag = document.getElementsByClassName("hashtag");
+
+function init() {
+    for(let i = 0; i<$hashtag.length; i++){
+        $hashtag[i].addEventListener("click", )
+    }
+}
