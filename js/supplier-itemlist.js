a2422f6a319251ef12385b5355a4567cf2c01975
jiwon <jiwon_3261@naver.com>
2021-04-24 22:33:01 +0900

    @업체-상품리스트-작업완료

---
+++ js/supplier-itemlist.js
@@ -0,0 +1,43 @@
+(() => {
+  const tableElem = document.getElementById("item-list-table");
+
+  function clickHandler(e) {
+    let elem = e.target;
+
+    console.log(elem)
+
+    if (elem.classList.contains("item-img-inTable")){
+        console.log(elem.getAttribute("src"))
+        window.open(`http://127.0.0.1:5500/4TH-Project/${elem.getAttribute("src")}`);
+
+        return;
+    }
+
+    if (elem.nodeName === "INPUT") {
+      // alert("체크박스")
+      console.log(elem);
+      if (elem.classList.contains("item-all-check")) {
+        let checkBoxArr = document.getElementsByClassName("item-check");
+        for (let i = 0; i < checkBoxArr.length; i++) {
+          checkBoxArr[i].checked = elem.checked;
+        }
+      } else {
+        alert(`${elem.parentNode.parentNode.dataset.itemidx} 행 체크 클릭`);
+      }
+      return;
+    }
+
+    while (!elem.classList.contains("item-row")) {
+      elem = elem.parentNode;
+      if (elem.nodeName === "TABLE") {
+        elem = null;
+        console.log("tabedfsdfsdf");
+        return;
+      }
+    }
+
+    alert(`${elem.dataset.itemidx} 행 클릭`);
+  }
+
+  tableElem.addEventListener("click", clickHandler);
+})();
