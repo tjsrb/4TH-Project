4106e1af97b2e22026ee03873d335ed0a6b2dd5b
answhdgkr95 <49180647+answhdgkr95@users.noreply.github.com>
2021-05-10 11:36:37 +0900

    @이미지 수정, 해시테그 버튼 기능완료

--- normal-user/Recently-store/recently-store.js
+++ normal-user/Recently-store/recently-store.js
@@ -11,6 +11,10 @@ const $skirt = document.getElementsByClassName('skirt');
 const $onepice = document.getElementsByClassName('onepice');
 const $twopice = document.getElementsByClassName('twopice');
 const $jaket = document.getElementsByClassName('jaket');
+const $btnTag = document.getElementsByClassName('btnTag');
+
+// $btnTag.addEventListener("click",);
+
 
 $btnshirt.addEventListener("click",btnshirtClickEvent);
 $btnpants.addEventListener("click",btnpantsClickEvent);
@@ -19,61 +23,132 @@ $btnonepice.addEventListener("click",btnonepiceClickEvent);
 $btntwopice.addEventListener("click",btntwopiceClickEvent);
 $btnjaket.addEventListener("click",btnjaketClickEvent);
 
+
+let j = 0; 
+function jHandler(){
+if(j == 0){
+   j++;
+}
+else{
+    j = 0 ;
+}
+}
 function btnshirtClickEvent (){
     DisplaynoneALL();
-    for(let i=0; i<$shirt.length; i++){
-        $shirt[i].style.display ="block";
+    jHandler();
+    if(j == 1){
+        for(let i=0; i<$shirt.length; i++){
+            $shirt[i].style.display ="block";
+        }
     }
+    else if (j == 0){
+        ShowALL();
+    } 
 }
+
+
+
+
 
 function btnpantsClickEvent(){
     DisplaynoneALL();
-    for(let i=0; i<$pants.length; i++){
-        $pants[i].style.display ="block";
+    jHandler();
+    if(j == 1){
+        for(let i=0; i<$pants.length; i++){
+            $pants[i].style.display ="block";
+        }
     }
+    else if (j == 0){
+        ShowALL();
+    } 
 }
 function btnskirtClickEvent(){
     DisplaynoneALL();
-    for(let i=0; i<$skirt.length; i++){
-        $skirt[i].style.display ="block";
+    jHandler();
+    if(j == 1){
+        for(let i=0; i<$skirt.length; i++){
+            $skirt[i].style.display ="block";
+        }
     }
+    else if (j == 0){
+        ShowALL();
+    } 
 }
 function btnonepiceClickEvent(){
     DisplaynoneALL();
-    for(let i=0; i<$onepice.length; i++){
-        $onepice[i].style.display ="block";
+    jHandler();
+    if(j == 1){
+        for(let i=0; i<$onepice.length; i++){
+            $onepice[i].style.display ="block";
+        }
     }
+    else if (j == 0){
+        ShowALL();
+    } 
 }
 function btntwopiceClickEvent(){
     DisplaynoneALL();
-    for(let i=0; i<$twopice.length; i++){
-        $twopice[i].style.display ="block";
+    jHandler();
+    if(j == 1){
+        for(let i=0; i<$twopice.length; i++){
+            $twopice[i].style.display ="block";
+        }
     }
+    else if (j == 0){
+        ShowALL();
+    } 
 }
 function btnjaketClickEvent(){
     DisplaynoneALL();
-    for(let i=0; i<$jaket.length; i++){
-        $jaket[i].style.display ="block";
+    jHandler();
+    if(j == 1){
+        for(let i=0; i<$jaket.length; i++){
+            $jaket[i].style.display ="block";
+        }
     }
+    else if (j == 0){
+        ShowALL();
+    } 
 }
 
 function DisplaynoneALL(){
-    for(let i =0; i = $shirt.length; i++){
+    for(let i =0; i < $shirt.length; i++){
         $shirt[i].style.display = "none";
     }
-    for(let i =0; i = $pants.length; i++){
+    for(let i =0; i < $pants.length; i++){
         $pants[i].style.display = "none";
     }
-    for(let i =0; i = $skirt.length; i++){
+    for(let i =0; i < $skirt.length; i++){
         $skirt[i].style.display = "none";
     }
-    for(let i =0; i = $onepice.length; i++){
+    for(let i =0; i < $onepice.length; i++){
         $onepice[i].style.display = "none";
     }
-    for(let i =0; i = $twopice.length; i++){
+    for(let i =0; i < $twopice.length; i++){
         $twopice[i].style.display = "none";
     }
-    for(let i =0; i = $jaket.length; i++){
+    for(let i =0; i < $jaket.length; i++){
         $jaket[i].style.display = "none";
+    }
+}
+
+function ShowALL(){
+    for(let i=0; i<$shirt.length; i++){
+        $shirt[i].style.display ="block";
+    }
+    for(let i=0; i<$pants.length; i++){
+        $pants[i].style.display ="block";
+    }
+    for(let i=0; i<$skirt.length; i++){
+        $skirt[i].style.display ="block";
+    }
+    for(let i=0; i<$onepice.length; i++){
+        $onepice[i].style.display ="block";
+    }
+    for(let i=0; i<$twopice.length; i++){
+        $twopice[i].style.display ="block";
+    }
+    for(let i=0; i<$jaket.length; i++){
+        $jaket[i].style.display ="block";
     }
 }
